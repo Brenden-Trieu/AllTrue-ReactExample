0bf4c4d132028e2bb248e149baec487e4a47e026
Brenden T <brenden.trieu@gmail.com>
2024-04-01 22:47:52 -0700

    incomplete

--- reactapp/src/components/App.js
+++ reactapp/src/components/App.js
@@ -1,7 +1,34 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
 function App() {
+  const [data, setData] = useState();
+
+  const fetchData = async () => {
+    try {
+      const response = await axios
+        .get("https://dummy.restapiexample.com/api/v1/employees")
+        .setData(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  // if (!data) {
+  //   return <div>loading</div>;
+  // }
+
   return (
     <div className="App">
-      <p>hello world</p>
+      {data.map((employee) => (
+        <div key={employee.id}>
+          <p>{employee.name}</p>
+        </div>
+      ))}
     </div>
   );
 }
